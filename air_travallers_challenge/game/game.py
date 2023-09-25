32259e7d08a5a5dd7f2ab28ed754d07b958ce824
Konsta00 <konsta.korpela10@gmail.com>
2023-09-25 18:27:06 +0300

    Update game.py

--- air_travallers_challenge/game/game.py
+++ air_travallers_challenge/game/game.py
@@ -3,21 +3,26 @@ import random
 from database.db_models import get_closest_airports
 
 class Game:
+# INIATIALIZE GAME CLASS 
     def __init__(self):
         self.game_over = False
         self.closest_airports = None
         self.player = None
         self.current_airport = None
 
+# SET GAME INSTANCE PLAYER
     def set_player(self, player):
         self.player = player
 
+# SET GAME CURRENT AIRPORT TO PLAYER AIRPORT (CURRENT)
     def set_current_airport(self):
         self.current_airport = self.player.airport
 
+# SET 10 CLOSEST AIRPORT TO GAME FROM DATABASE QUERY 
     def set_closest_airports(self):
         self.closest_airports = get_closest_airports(self.current_airport)
 
+# PRINT AVATARS ON SCREEN
     def print_avatars(self):
         avatars = ('Donald Trump', 'Mona Lisa', 'Felipe VI')
 
@@ -26,6 +31,7 @@ class Game:
         for index, avatar in enumerate(avatars):
             print(f'{index+1}. {avatar}')
 
+# UPDATE GAME WITH NEW VALUES
     def update_game(self, status, player, next_airport, next_closest_airports):
         self.game_over = status
         self.current_airport = next_airport
